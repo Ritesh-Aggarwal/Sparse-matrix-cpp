6cbda567e57d3d71f0a02048176c7f6cbca8ea84
Ritesh-Aggarwal <57039447+Ritesh-Aggarwal@users.noreply.github.com>
2021-08-07 12:31:10 +0530

    overload + operator

--- code.cpp
+++ code.cpp
@@ -2,7 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FIO                           \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(0);                       \
+	cout.tie(0);
 
 #define p 1000000007
 #define ll long long int
@@ -13,11 +16,13 @@ struct element
 	int i;
 	int j;
 	int x;
-	inline void display() {
+	inline void display()
+	{
 		cout << i << " " << j << " " << x << "\n";
 	}
 
-	element(int a, int b, int c) {
+	element(int a, int b, int c)
+	{
 		i = a;
 		j = b;
 		x = c;
@@ -31,9 +36,10 @@ private:
 	int n;
 	int nums;
 	vector<element> e;
-public:
 
-	void create() {
+public:
+	void create()
+	{
 		cin >> m;
 		cin >> n;
 		cin >> nums;
@@ -43,25 +49,47 @@ public:
 			int a, b, c;
 			cin >> a >> b >> c;
 			// element ele(a, b, c);
-			e.push_back(element (a, b, c));
+			e.push_back(element(a, b, c));
 			i++;
 		}
 	}
-	void display() {
-		cout << "list details: \n";
-		cout << "number of rows :" << this->m; cout << "\n";
-		cout << "number of columns:" << this->n; cout << "\n";
-		cout << "number of non-zero elements:" << this->nums; cout << "\n";
+	void createByMatrix(vector<vector<int>> &matrix)
+	{
+		m = matrix.size();
+		n = matrix[0].size();
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (matrix[i][j] != 0)
+				{
+					e.push_back(element (i + 1, j + 1, matrix[i][j]));
+				}
+			}
+		}
+		nums = e.size();
+	}
+	void display()
+	{
+		cout << "number of rows :" << this->m;
+		cout << "\n";
+		cout << "number of columns:" << this->n;
+		cout << "\n";
+		cout << "number of non-zero elements:" << this->nums;
+		cout << "\n";
 		for (int i = 0; i < this->e.size(); ++i)
 		{
 			this->e[i].display();
 		}
+		cout << "\n";
 	}
-	void displayMatrix() {
+	void displayMatrix()
+	{
 
-		vector<vector<int>> vec( n , vector<int> (m));
+		vector<vector<int>> vec(n, vector<int>(m));
 
-		for (int k = 0; k < e.size(); k++) {
+		for (int k = 0; k < e.size(); k++)
+		{
 			int r = e[k].i;
 			int c = e[k].j;
 			vec[r - 1][c - 1] = e[k].x;
@@ -75,27 +103,47 @@ public:
 			}
 			cout << endl;
 		}
+		cout << "\n";
 	}
-	void createByMatrix(vector< vector<int> > &matrix) {
-		m = matrix.size();
-		n = matrix[0].size();
-		e.push_back(element (m, n, 0));
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (matrix[i][j] != 0) {
-					e.push_back(element (i + 1, j + 1, matrix[i][j]));
+	sparse operator + (sparse const &obj) {
+		int a = 0, b = 0;
+		if (m == obj.m && n == obj.n) {
+			sparse result(m, n);
+			while (a < e.size() || b < obj.e.size()) {
+				if (e[a].i == obj.e[b].i) {
+					if (e[a].j == obj.e[b].j) {
+						result.e.push_back(element (e[a].i, e[a].j, e[a].x + obj.e[b].x));
+						a++;
+						b++;
+					} else if (e[a].j < obj.e[b].j) {
+						result.e.push_back(element (e[a].i, e[a].j, e[a].x));
+						a++;
+					} else {
+						result.e.push_back(element (e[a].i, obj.e[b].j, obj.e[b].x));
+						b++;
+					}
+				} else if (e[a].i < obj.e[b].i) {
+					result.e.push_back(element (e[a].i, e[a].j, e[a].x));
+					a++;
+				} else {
+					result.e.push_back(element (obj.e[b].i, obj.e[b].j, obj.e[b].x));
+					b++;
 				}
 			}
-		}
-		nums = e.size() - 1;
-		e[0].x = nums;
+			result.nums = result.e.size();
+			return result;
+		} else return sparse (1, 1);
 
+	}
+
+	sparse() {}
+	sparse(int a, int b) {
+		m = a; n = b; nums = 0;
 	}
 };
 
-
-
-int main() {
+int main()
+{
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
@@ -104,21 +152,23 @@ int main() {
 
 	sparse A;
 	A.create();
-	A.display();
+	// A.display();
 	A.displayMatrix();
 
-	vector< vector<int>> matrix1 {
+	vector<vector<int>> matrix1{
 		{1, 0, 0},
-		{0, 2, 4},
-		{0, 0, 3}
-	};
+		// {1, 2, 0},
+		{0, 2, 5},
+		{0, 0, 3}};
 	sparse B;
 	B.createByMatrix(matrix1);
-	B.display();
+	// B.display();
 	B.displayMatrix();
 
+
+	sparse C;
+	C = A + B;
+	// C.display();
+	C.displayMatrix();
 	return 0;
 }
-
-
-
